bdfc688e6f7d6c2866d09db1bf243a880aa47c8b
lolobog <bogdanseb01@yahoo.com>
2020-11-11 12:18:07 +0000

    Scenes+2 Scripts

---
+++ Week 4 Theme Decreasing/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public static float currentTime = 0f;
+    float startingTime = 10f;
+
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        print(currentTime);
+        if (currentTime <= 0)
+            SceneManager.LoadScene("Lose");
+
+    }
+}
---
+++ Week 4 Theme Decreasing/Assets/Scripts/item_click.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item_click : MonoBehaviour
+{
+    public static string name_of_obj;
+    public GameObject obj_name_text;
+
+    private void OnMouseDown()
+    {
+        name_of_obj = gameObject.name;
+        //Debug.Log(name_of_obj);
+        Destroy(gameObject);
+        Destroy(obj_name_text);
+    }
+
+}
